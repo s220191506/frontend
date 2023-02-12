f84f9e2e96f306c314285ecc90cd6861b22a1b81
kingsly <s220191506@mandela.ac.za>
2023-02-12 10:59:52 +0200

    INIT

---
+++ src/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { QuestionList } from './QuestionsList';
+import { getUnansweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+export const HomePage = () => (
+  <Page>
+    <div>
+      <PageTitle>Unanswered Questions</PageTitle>
+      <button>Ask a question</button>
+    </div>
+    <QuestionList
+      data={getUnansweredQuestions()}
+      renderItem={(question) => <div>{question.title}</div>}
+    />
+  </Page>
+);
